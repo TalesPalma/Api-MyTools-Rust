73687e99a1849b62869dfc9ac5c15dba9035a519
TalesPalma <tales.palma_2022@outlook.com>
2024-05-13 22:47:23 -0300

    Aprendemos a implementar o post

--- src/main.rs
+++ src/main.rs
@@ -1,60 +1,43 @@
 use axum::{
-    body::Body,
-    http::{status, StatusCode},
-    response::{IntoResponse, Response},
-    routing::{get, post},
-    Json, Router,
+    http::StatusCode, response::IntoResponse, routing::get, Json, Router
 };
-use serde::{Deserialize, Serialize};
+use serde::{Deserialize,Serialize};
+
+
+
+static mut TOOLS: Vec<Tools> = Vec::new();
+
+
+#[derive(Deserialize,Clone,Serialize)]
+struct Tools {
+    name: String,
+    description: String,
+    url: String,
+    empresa: String,
+}
+
+async fn add_item(Json(tool): Json<Tools>) -> impl IntoResponse {
+    unsafe {
+        TOOLS.push(tool.clone());
+    }
+    StatusCode::CREATED
+}
+
+
+async fn tools_get() -> Json<Vec<Tools>> {
+    unsafe { Json(TOOLS.clone()) }
+}
+
+
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new()
-        .route("/", get(root))
-        .route("/tools",get(tools));
+    let app = Router::new().route("/tools", get(tools_get).post(add_item));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
-
 }
 
-#[derive(Serialize)]
-struct Tools{
-    name: String,
-    description: String,
-    url: String,
-    empresa: String
-}
-
-// async fn create_tools() -> impl IntoResponse{
-//     Response::builder()
-//         .status(StatusCode::CREATED)
-//         .body(Body::from("create tools"))
-//         .unwrap()
-// }
-
-async fn tools() -> Json<Vec<Tools>>{
-    let tools = vec![
-        Tools{
-            name: "name".to_string(),
-            description: "description".to_string(),
-            url: "url".to_string(),
-            empresa: "empresa".to_string()
-        },
-        Tools{
-            name: "name".to_string(),
-            description: "description".to_string(),
-            url: "url".to_string(),
-            empresa: "empresa".to_string()
-        }
-    ];
-        Json(tools)
-}
-
-
-async fn root() -> &'static str {
-    "Hello, World!"
-}
 
